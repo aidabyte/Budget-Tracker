3689ae6c0f8b4135503c16009203b3c3d3d4413d
aidabyte <ablinstrubyte@gmail.com>
2020-11-15 22:35:42 -0600

    changed path for cache storage

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,9 +5,8 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/styles.css",
-  "/manifest.webmanifest",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
  
 ];
 
